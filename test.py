0f4969f1744ea32dbb1ac590836255e81233daa5
LilB333 <renal_turaev@mail.ru>
2025-08-29 12:22:56 +0300

    -Добавил time.sleep для удобства -Добавил скролл до элементов

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -26,15 +27,26 @@ try:
     driver.get(url)
 except Exception as e:
     driver.execute_script("window.stop();")  # Останавливаем загрузку
+driver.maximize_window()
 
-# Создаем ActionChains
+#Создаем ActionChains
 actions = ActionChains(driver)
 
+#Создаем переменную time_sleep для удобства проверки работы автотеста
+time_sleep = 5
+
 #ПРОВЕРЯЕМ ЧЕК-БОКС ORANGE
-checkbox = driver.find_element(By.XPATH, "//h2[@class='title' and contains(text(),'By Default Selected Check Box option')]")
-checkbox_element = driver.find_element(By.XPATH, "//input[@id='checkbox1' and @value='orange']")
-assert checkbox_element.is_selected()
+checkbox_element_1 = driver.find_element(By.XPATH, "//input[@id='checkbox1' and @value='orange']")
+#Скроллим чуть чекбокса
+driver.execute_script(
+        "arguments[0].scrollIntoView({block: 'center'});"  
+        "window.scrollBy(0, 50);",  #Опускаем на 50px вниз
+        checkbox_element_1
+)
+assert checkbox_element_1.is_selected()
 print('Чек-бокс Orange заполнен')
+
+time.sleep(time_sleep)
 
 #ЗАПОЛНЯЕМ ЧЕК-БОКС BLUE
 checkbox_element_2 = driver.find_element(By.XPATH, "//input[@id='checkbox2' and @value='blue']")
@@ -42,11 +54,15 @@ checkbox_element_2.click()
 assert checkbox_element_2.is_selected()
 print('Чек-бокс Blue заполнен')
 
+time.sleep(time_sleep)
+
 #КЛИКАЕМ НА ССЫЛКУ Open a popup window
 link_element =  driver.find_element(By.XPATH, "//a[contains(text(), 'Open a popup window')]")
 base_window = driver.current_window_handle
 windows_before_click = driver.window_handles
 link_element.click()
+
+time.sleep(time_sleep)
 
 #ПРОВЕРЯЕМ ОТОБРАЖЕНИЕ НОВОГО ОКНА
 WebDriverWait(driver, 10).until(lambda x: len(x.window_handles) > len(windows_before_click))
@@ -57,17 +73,30 @@ driver.switch_to.window(new_window)
 assert driver.current_url != url
 print("Открыто новое окно")
 
+time.sleep(time_sleep)
+
 #ЗАКРЫВАЕМ ОКНО
 driver.close()
 print("Окно закрыто")
 #Переключаемся на главное окно
 driver.switch_to.window(base_window)
 
+time.sleep(time_sleep)
+
 #КЛИКАЕМ ДВАЖДЫ НА КНОПКУ Double-click
 double_click_element = driver.find_element(By.XPATH, "//p[@id='testdoubleclick']")
 actions.double_click(double_click_element).perform()
-assert driver.find_element(By.XPATH, "//div[@class='dropdown-content show']")
+context_menu = driver.find_element(By.XPATH, "//div[@class='dropdown-content show']")
+#Скроллим чуть ниже контекстного меню
+driver.execute_script(
+        "arguments[0].scrollIntoView({block: 'center'});"  
+        "window.scrollBy(0, 50);",  #Опускаем на 50px вниз
+        context_menu
+)
+assert context_menu.is_displayed()
 print("Отображается контекстное меню")
+
+time.sleep(time_sleep)
 
 #КЛИКАЕМ НА Gmail В КОНТЕКСТНОМ МЕНЮ
 gmail_button = driver.find_element(By.XPATH, "//a[contains(text(),'Gmail')]")
@@ -78,10 +107,15 @@ except Exception as e:
 assert driver.current_url != url
 print("Открыта новая страница")
 
+time.sleep(time_sleep)
+
 #ВОЗВРАЩАЕМСЯ ОБРАТНО НА СТРАНИЦУ https://omayo.blogspot.com/
 try:
     driver.back()
 except Exception as e:
     driver.execute_script("window.stop();")  # Останавливаем загрузку
 print("Вернулись обратно на https://omayo.blogspot.com/")
+
+time.sleep(time_sleep)
+
 driver.quit()
